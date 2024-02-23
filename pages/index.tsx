ba943a2496b6762f3aabb2f248def4ac0450c862
Mgs <akerberganders@gmail.com>
2024-02-23 10:10:57 +0000

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home() {
 
   const getBleDevices = useCallback(async () => {
     let response = await populateBluetoothDevices();
-    console.log(response);
     setDevices(response);
   }, []);
 
