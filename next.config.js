2d115ae0281a4e48472a03d54d158a1f23118d34
Mgs <akerberganders@gmail.com>
2024-02-23 11:07:43 +0000

    fix

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ module.exports = withPWA({
   swcMinify: true,
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/public',
+  basePath: '/pwa-ble-testapp',
   // webpack: (config) => {
   //   config.resolve.fallback = {
   //     fs: false,
